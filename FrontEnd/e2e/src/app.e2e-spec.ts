8aca0a76f5cbcf8ea456cf58d3c10df5b948e9b1
Aldiyar <adelov.aldiyar@gmail.com>
2021-03-28 23:39:55 +0600

    FrontEnd

--- FrontEnd/e2e/src/app.e2e-spec.ts
+++ FrontEnd/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('project app is running!');
+    expect(await page.getTitleText()).toEqual('FrontEnd app is running!');
   });
 
   afterEach(async () => {
