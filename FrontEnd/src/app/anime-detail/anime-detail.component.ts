d203ef76388bafcf8aa3dd04afb597ee08a818e1
Amir2002-kz <almabekov02@mail.ru>
2021-04-19 00:31:43 +0500

    '19042021'

--- FrontEnd/src/app/anime-detail/anime-detail.component.ts
+++ FrontEnd/src/app/anime-detail/anime-detail.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute,Router } from '@angular/router';
+import {Anime} from '../models/anime'
+import {anime} from "../models/anime_s"
 @Component({
   selector: 'app-anime-detail',
   templateUrl: './anime-detail.component.html',
   styleUrls: ['./anime-detail.component.css']
 })
 export class AnimeDetailComponent implements OnInit {
+  anime_detail!:Anime;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private router: Router) { }
 
   ngOnInit(): void {
+    this.anime_detail = anime.find(anime_detail => anime_detail.id === Number(this.route.snapshot.paramMap.get('anime_id')));
   }
 
 }
