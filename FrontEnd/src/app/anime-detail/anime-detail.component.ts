5bfcd6a3c6f1a54680475fac41544572836a13c2
Aldiyar <adelov.aldiyar@gmail.com>
2021-04-19 02:19:34 +0600

    FrontEnd

--- FrontEnd/src/app/anime-detail/anime-detail.component.ts
+++ FrontEnd/src/app/anime-detail/anime-detail.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
-import {Anime} from '../models/anime'
-import {anime} from "../models/anime_s"
+import { ActivatedRoute, Router } from '@angular/router';
+import {Anime} from '../models/anime';
+import {animeList} from '../models/anime_s';
 @Component({
   selector: 'app-anime-detail',
   templateUrl: './anime-detail.component.html',
   styleUrls: ['./anime-detail.component.css']
 })
 export class AnimeDetailComponent implements OnInit {
-  anime_detail!:Anime;
+  anime: Anime = undefined;
 
-  constructor(private route: ActivatedRoute,private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.anime_detail = anime.find(anime_detail => anime_detail.id === Number(this.route.snapshot.paramMap.get('anime_id')));
+    this.loadAnime();
   }
-
+  loadAnime(): void{
+    this.route.paramMap.subscribe((param) => {
+      const id = +param.get('anime_id');
+      console.log(id);
+      this.anime = animeList.find(x => x.id === id);
+      console.log(this.anime);
+    });
+  }
 }
