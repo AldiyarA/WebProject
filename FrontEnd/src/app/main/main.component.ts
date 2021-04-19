bb1bc0f17b7bd4c9d416d2375b5e8cb3b6f0f24a
Aldiyar <adelov.aldiyar@gmail.com>
2021-04-20 00:20:46 +0600

    FrontEnd

--- FrontEnd/src/app/main/main.component.ts
+++ FrontEnd/src/app/main/main.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Anime} from '../models/anime';
-import {Genre} from '../models/genre';
 import {GenreService} from '../services/genre.service';
 
 @Component({
@@ -9,22 +7,8 @@ import {GenreService} from '../services/genre.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  animeList: Anime[] = [];
-  genres: Genre[] = [];
-  genre: Genre = undefined;
   constructor(private genreService: GenreService) { }
 
   ngOnInit(): void {
-    this.loadGenres();
-  }
-
-  private loadGenres(): void{
-    this.genreService.getGenres().subscribe(genres => {this.genres = genres; });
-    this.genreService.getGenre(1).subscribe(genre => {
-      this.genre = genre;
-      this.genreService.getAnimeList(genre.id).subscribe(animeList => {
-        this.animeList = animeList;
-      });
-    });
   }
 }
