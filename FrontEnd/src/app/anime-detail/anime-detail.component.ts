594ed08df091074f54cfa220f184225a91330341
Aldiyar <adelov.aldiyar@gmail.com>
2021-04-20 18:00:07 +0600

    Front

--- FrontEnd/src/app/anime-detail/anime-detail.component.ts
+++ FrontEnd/src/app/anime-detail/anime-detail.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {Anime} from '../models/anime';
-import {animeList} from '../models/anime_s';
 import {Article} from '../models/article';
-import {articles} from '../models/articles';
 import {Genre} from '../models/genre';
-import {genres} from '../models/genres';
 import {Character} from '../models/character';
-import {characters} from '../models/characters';
+import {AnimeService} from '../services/anime.service';
 @Component({
   selector: 'app-anime-detail',
   templateUrl: './anime-detail.component.html',
@@ -18,7 +15,7 @@ export class AnimeDetailComponent implements OnInit {
   articles: Article[];
   genres: Genre[];
   characters: Character[];
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router, private animeService: AnimeService) { }
 
   ngOnInit(): void {
     this.loadAnime();
@@ -26,23 +23,30 @@ export class AnimeDetailComponent implements OnInit {
   loadAnime(): void{
     this.route.paramMap.subscribe((param) => {
       const id = +param.get('anime_id');
-      console.log(id);
-      this.anime = animeList.find(x => x.id === id);
-      console.log(this.anime);
-      this.loadArticle();
-      this.loadGenre();
-      this.loadCharacters();
+      this.animeService.getAnime(id).subscribe(anime => {
+        this.anime = anime;
+        this.loadArticle();
+        this.loadGenre();
+        this.loadCharacters();
+      });
     });
   }
   loadArticle(): void{
-    this.articles = articles;
+    this.animeService.getArticles(this.anime.id).subscribe(articles => {
+      this.articles = articles;
+    });
   }
   loadGenre(): void{
-    this.genres = genres;
+    this.animeService.getGenres(this.anime.id).subscribe(genres => {
+      this.genres = genres;
+    });
   }
   loadCharacters(): void{
-    this.characters = characters;
+    this.animeService.getCharacters(this.anime.id).subscribe(characters => {
+      this.characters = characters;
+    });
   }
   updateArticle(article: Article): void{
+    this.animeService.updateArticle(this.anime.id, article);
   }
 }
