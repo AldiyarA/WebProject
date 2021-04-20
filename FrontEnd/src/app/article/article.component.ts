1350fc3b50b472e4ec339df19cbb6ccfd70d40da
Amir2002-kz <almabekov02@mail.ru>
2021-04-20 16:30:29 +0500

    FrontEnd

--- FrontEnd/src/app/article/article.component.ts
+++ FrontEnd/src/app/article/article.component.ts
@@ -9,12 +9,17 @@ import {Article} from '../models/article';
 export class ArticleComponent implements OnInit {
   @Input() article: Article = undefined;
   @Output() update = new EventEmitter();
-
+  managing = false;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.article.content.length)
+  }
+  checkLength(len1: number, len2: number): boolean{
+    return this.article.content !== null && this.article.content.length >= len1 && this.article.content.length < len2;
   }
   save(): void{
+    this.managing=false;
     this.update.emit(this.article);
   }
 }
